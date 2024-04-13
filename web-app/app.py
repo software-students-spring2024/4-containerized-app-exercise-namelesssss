e2e01710fe611fa94bb00875d11288b693df7fdf
Kalados <kl3890@nyu.edu>
2024-04-13 15:40:23 -0400

    working docker-compose'd version w bugs

--- web-app/app.py
+++ web-app/app.py
@@ -1,25 +1,28 @@
 from flask import Flask, request, jsonify, render_template
-import requests
-
+from machineClient.grammar_check import check_grammar
+from machineClient.db import store_results
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    if request.method == 'POST':
-        passage = request.form['userInput']
-        response = requests.post("http://back-end:5002", data={'passage':passage})
-    return render_template('home.html')
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
-
-'''
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        passage = request.form['passage']
-        original_passage, fixed_passage, error_analysis = check_grammar(passage)
-        return render_template('home.html', fixed_passage=fixed_passage, error_analysis=error_analysis)
+        passage = request.form['userInput']
+        original_passage, fixed_passage, error_analysis, api_response = check_grammar(passage)
+        store_results(original_passage, fixed_passage, error_analysis, api_response)
+        return render_template('home.html', passage=original_passage, fixed_passage=fixed_passage, error_analysis=error_analysis)
     return render_template('home.html')
-'''
+
+@app.route("/analyze", methods=["POST"])
+def analyze_passage():
+    try:
+        passage = request.json["passage"]
+    except KeyError:
+        return jsonify({"error": "Missing 'passage' key in the request payload"}), 400
+
+    original_passage, fixed_passage, error_analysis, api_response = check_grammar(passage)
+    store_results(original_passage, fixed_passage, error_analysis, api_response)
+    return jsonify({"fixed_passage": fixed_passage, "error_analysis": error_analysis})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5001, debug=True)
