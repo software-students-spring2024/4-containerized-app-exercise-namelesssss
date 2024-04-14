9e8558e11b66dc93f6b9adaf481b90535a707c6e
hah8236 <hah8236@nyu.edu>
2024-04-14 13:17:28 -0400

    working on web app tests

--- tests/web_app_test.py
+++ tests/web_app_test.py
@@ -1,19 +1,5 @@
-# Web App Tests
-# Coveage Progress: 0%
 import pytest
 from web-app.app import transcribe_audio, home, analyze_passage, transcribe
-
-# Web app tests!!
-
-# ** STRUCUTRE **
-# def test_namey_name():
-#   do something
-#   more something
-# assert THING == THING2
-# repeat assert things until every part of that portion is tested
-
-# I want peer jiuce = I want pear juice
-# Who am you = who are you\
 
 def test_transcribe_audio():
     file_path = "path"
@@ -22,10 +8,9 @@ def test_transcribe_audio():
     print("transcribe_audio test has passed! ")
 
 
-def test_home_page():
-    # testing the homepage
-    # return render_template("home.html") 
-    home = "uhhh"
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
     print("home page test has passed!")
 
 
@@ -38,49 +23,12 @@ def test_analyze_passage():
 def test_transcribe():
     # return jsonify({"transcript": transcript})
     # transcribes the audio file
-    audio_file = "uhh"
+    original_passage = "This is a test passage."
     print("transcribe test has passed!")
 
-# might not need after all...
-'''
-def test_text_no_mistakes():
-    passage = "I am perfect."
-    expected_passage = "I am perfect."
-    expected_error_analysis = {
-        "spelling": 0,
-        "verb/tense": 0,
-        "article/preposition": 0,
-        "other": 0
-    }
-    # assert expected_passage = [GETTING THE OUTCOME FOR THAT]
-    # assert expected_error_analysis = [GETTING THE OUTCOME FOR THAT PT 2]
 
-
-def test_text_mistakes():
-    passage = "He be runnin."
-    expected_passage = "He is running."
-    # expected_error_analysis = 
-    # assert expected_passage = [GETTING THE OUTCOME FOR THAT]
-    # assert expected_error_analysis = [GETTING THE OUTCOME FOR THAT PT 2]
-
-
-def test_voice_no_mistakes():
-    # test a sound file too, not just via microphone
-    expected_heard = "I am perfect."
-    expected_passage = "I am perfect."
-    # expected_error_analysis = 
-    # assert expected_heard = the speech to text
-    # assert expected_passage = [GETTING THE OUTCOME FOR THAT]
-    # assert expected_error_analysis = [GETTING THE OUTCOME FOR THAT PT 2]
-
-
-def test_voice_mistakes():
-    # test a sound file too, not just via microphone
-    expected_heard = "Who am you?"
-    expected_passage = "Who are you?"
-    # expected_error_analysis = 
-    # assert expected_heard = the speech to text
-    # assert expected_passage = [GETTING THE OUTCOME FOR THAT]
-    # assert expected_error_analysis = [GETTING THE OUTCOME FOR THAT PT 2]
-
-'''
+if __name__ == "__main__":
+    test_transcribe()
+    test_analyze_passage()
+    test_home_page()
+    test_transcribe_audio()
