78868942efcecba76a2663b05729fc899ee17733
hah8236 <hah8236@nyu.edu>
2024-04-14 11:06:21 -0400

    fixing merge issues

--- web-app/app.py
+++ web-app/app.py
@@ -7,23 +7,14 @@ Web applicaiton for our machine learning client that takes in text and corrects 
 from flask import Flask, request, jsonify, render_template
 from machineClient.grammar_check import check_grammar
 from machineClient.db import store_results
-<<<<<<< HEAD
 from google.cloud import speech
+# from google.cloud import speech_v1p1beta1 as speech
 
-=======
-from google.cloud import speech_v1p1beta1 as speech
->>>>>>> 2d893df98db909ef669f652dc41840a8de3003fd
 app = Flask(__name__)
 """
 home page of the web app
 """
-<<<<<<< HEAD
-
-
-def transcribe_audio(file_path):
-=======
 def transcribe_audio(audio_file):
->>>>>>> 2d893df98db909ef669f652dc41840a8de3003fd
     client = speech.SpeechClient()
     content = audio_file.read()
     audio = speech.RecognitionAudio(content=content)
@@ -35,14 +26,8 @@ def transcribe_audio(audio_file):
     response = client.recognize(config=config, audio=audio)
     transcript = ""
     for result in response.results:
-<<<<<<< HEAD
-        return format(result.alternatives[0].transcript)
-
-
-=======
         transcript += format(result.alternatives[0].transcript) + " "
     return transcript.strip()
->>>>>>> 2d893df98db909ef669f652dc41840a8de3003fd
 @app.route("/", methods=["GET", "POST"])
 def home():
     "Home page of the web app."
